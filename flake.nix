66cc439b4211b2fd7b37b0320c62d962ca8657d2
PeDro0210 <pedroruben021@gmail.com>
2025-08-31 11:57:21 -0600

    fix: removing flake-utils

--- flake.nix
+++ flake.nix
@@ -1,80 +1,65 @@
 {
 
-  # this is mostly just use for dev
-  description = "A nix flake for working with Bevy and Raylib ";
+  description = "Slash Maze flake nix";
 
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    naersk.url = "github:nix-community/naersk";
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      utils,
       naersk,
     }:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      naerskLib = pkgs.callPackages naersk { };
 
-    utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
+      libs = with pkgs; [
+        libGL
+        udev
+        alsa-lib
+        vulkan-loader
+        xorg.libX11
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXrandr # To use the x11 feature
+        libxkbcommon
+        wayland # To use the wayland feature
+        xorg.libXinerama
+        dbus
+      ];
 
-        libs = with pkgs; [
-          libGL
-          udev
-          alsa-lib # audio for bevy
-          vulkan-loader # fuck you vulkan shaders
-          xorg.libX11 # To use the x11 feature
-          xorg.libXcursor # To use the x11 feature
-          xorg.libXi # To use the x11 feature
-          xorg.libXrandr # To use the x11 feature
-          libxkbcommon
-          wayland # To use the wayland feature
-          xorg.libXinerama
-          dbus
-        ];
+      packages =
+        with pkgs;
+        [
+          glfw
+          cmake
+          clang
+          pkg-config
+          cargo
+          rustc
+          rust-analyzer
+          clippy
+          rustfmt
+        ]
+        ++ libs;
 
-        packages =
-          with pkgs;
-          [
-            glfw # for multi plataform
-            cmake # cause of raylib binding
-            clang # cause of raylib binding
-            pkg-config # cause of raylib binding
-          ]
-          ++ libs;
+    in
+    {
 
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage {
-          src = ./.;
-          buildInputs = libs;
-          nativeBuildInputs = packages;
-        };
+      packages."x86_64-linux".default = naerskLib.buildPackage {
+        src = ./.;
+        buildInputs = libs;
+        nativeBuildInputs = packages;
 
-        devShell =
-          with pkgs;
-          mkShell {
-            buildInputs = [
-              cargo
-              rustc
-              rustfmt
-              pre-commit
-              rustPackages.clippy
-            ];
+        LD_LIBRARY_PATH = libs;
 
-            RUST_SRC_PATH = rustPlatform.rustLibSrc;
-          };
+        LIBCLANG_PATH = "${pkgs.llvmPackages_15.libclang.lib}/lib";
+      };
 
-        default = {
-          src = ./.;
-        };
-
-      }
-    );
-
+      templates.default.path = ./.;
+    };
 }
