2e2a322124e132b31972fe222ecc2bbef3bbc0bd
PeDro0210 <pedroruben021@gmail.com>
2025-08-31 11:38:01 -0600

    fix: changing attribute for template to >2.18 standar

--- flake.nix
+++ flake.nix
@@ -17,67 +17,64 @@
       naersk,
     }:
 
-    utils.lib.eachDefaultSystem
-      (
-        system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-          naersk-lib = pkgs.callPackage naersk { };
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
 
-          libs = with pkgs; [
-            libGL
-            udev
-            alsa-lib # audio for bevy
-            vulkan-loader # fuck you vulkan shaders
-            xorg.libX11 # To use the x11 feature
-            xorg.libXcursor # To use the x11 feature
-            xorg.libXi # To use the x11 feature
-            xorg.libXrandr # To use the x11 feature
-            libxkbcommon
-            wayland # To use the wayland feature
-            xorg.libXinerama
-            dbus
-          ];
+        libs = with pkgs; [
+          libGL
+          udev
+          alsa-lib # audio for bevy
+          vulkan-loader # fuck you vulkan shaders
+          xorg.libX11 # To use the x11 feature
+          xorg.libXcursor # To use the x11 feature
+          xorg.libXi # To use the x11 feature
+          xorg.libXrandr # To use the x11 feature
+          libxkbcommon
+          wayland # To use the wayland feature
+          xorg.libXinerama
+          dbus
+        ];
 
-          packages =
-            with pkgs;
-            [
-              glfw # for multi plataform
-              cmake # cause of raylib binding
-              clang # cause of raylib binding
-              pkg-config # cause of raylib binding
-            ]
-            ++ libs;
+        packages =
+          with pkgs;
+          [
+            glfw # for multi plataform
+            cmake # cause of raylib binding
+            clang # cause of raylib binding
+            pkg-config # cause of raylib binding
+          ]
+          ++ libs;
 
-        in
-        {
-          defaultPackage = naersk-lib.buildPackage {
-            src = ./.;
-            buildInputs = libs;
-            nativeBuildInputs = packages;
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage {
+          src = ./.;
+          buildInputs = libs;
+          nativeBuildInputs = packages;
+        };
+
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              cargo
+              rustc
+              rustfmt
+              pre-commit
+              rustPackages.clippy
+            ];
+
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
           };
 
-          devShell =
-            with pkgs;
-            mkShell {
-              buildInputs = [
-                cargo
-                rustc
-                rustfmt
-                pre-commit
-                rustPackages.clippy
-              ];
-
-              RUST_SRC_PATH = rustPlatform.rustLibSrc;
-            };
-
-        }
-      )
-      {
         templates.default = {
-          path = ./template;
-          description = "A template for a Bevy + Raylib Rust project";
+          src = ./.;
         };
-      };
+
+      }
+    );
 
 }
